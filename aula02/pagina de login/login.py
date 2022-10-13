7715b30512aef9431af992daf4a16373cab823fb
= <joseeliodoro.ufrpe@gmail.com>
2022-10-13 16:39:27 -0300

    colocando função para o botão

--- aula02/pagina de login/login.py
+++ aula02/pagina de login/login.py
@@ -1,6 +1,4 @@
-from sqlite3 import dbapi2
 from tkinter import *
-from turtle import right
 
 #Criando janela
 root = Tk()
@@ -9,13 +7,16 @@ root.geometry('490x560+610+153')
 root.iconbitmap('ico.ico')
 root.resizable(True, True)
 
+def new_root():
+    root.destroy()
+
 #importar imagens
 img_fundo = PhotoImage(file='fundo.png')
 lb = Label(root, image=img_fundo)
 lb.pack()
 
 img_btn = PhotoImage(file='bt-img.png')
-btn = Button(lb, image=img_btn, cursor='hand2', bd=0)
+btn = Button(lb, image=img_btn, cursor='hand2', bd=0, command=new_root)
 btn.place(x=185, y=448)
 
 entry_id = Entry(lb, width=21, font='arial 25', bd=0)
@@ -24,6 +25,7 @@ entry_id.place(x=48, y=138)
 entry_email = Entry(lb, width=21, font='arial 25', bd=0, justify=RIGHT)
 entry_email.place(x=48, y=243)
 
-entry_password = Entry(lb, width=21, font='arial 25', bd=0)
-entry_password.place(x=485, y=354)
-root.mainloop()aaaaaaa
+entry_password = Entry(lb,show='*', width=21, font='arial 25', bd=0)
+entry_password.place(x=48, y=354)
+
+root.mainloop()
